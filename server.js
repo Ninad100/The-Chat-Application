b8c3afcca5dd854698964cb4299fc12cf60f6160
Ninad100 <63588506+Ninad100@users.noreply.github.com>
2024-09-03 18:27:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ import { server } from "./index.js";
 
 
 server.listen(3000,()=>{
-    console.log("Server is listening at 3000");
+    console.log("Server is listening at 3005");
     connectToDatabase();
 })
